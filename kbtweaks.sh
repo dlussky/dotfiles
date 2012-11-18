1c164dc104e4f8a5c285a1567c9646193357e2e6
Kirill Dlussky <kirill@dlussky.ru>
2012-11-19 00:07:56 +0400

    some comments

--- kbtweaks.sh
+++ kbtweaks.sh
@@ -1,3 +1,7 @@
+#making apple keyboard behave like ordinary one
+#inverse F## keys behaviour (now media only with fn)
 sh -c 'echo "2" > /sys/module/hid_apple/parameters/fnmode'
+#normal layout (tilda under escape)
 sh -c 'echo "0" > /sys/module/hid_apple/parameters/iso_layout'
+#swapping alt and command
 setxkbmap -option altwin:swap_lalt_lwin
