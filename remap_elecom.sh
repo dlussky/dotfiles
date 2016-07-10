100b4fe7fd67223e25d471f6a42ba30906632fb3
Kirill Dlussky <kirill@dlussky.ru>
2016-07-10 20:48:12 +0300

    some changes, yeah

--- remap_elecom.sh
+++ remap_elecom.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 #remapping back button to middle-click
-xinput set-button-map $(xinput | grep "ELECOM ELECOM TrackBall Mouse" | sed -e 's/.*id=\([0-9]\).*/\1/') 1 2 3 4 5 6 7 8 2 10 11 12 13 14 15 16
+xinput set-button-map $(xinput | grep "ELECOM ELECOM TrackBall Mouse" | sed -e 's/.*id=\([0-9]*\).*/\1/') 1 2 3 4 5 6 7 8 2 10 11 12 13 14 15 16
 
