491026038cb5121b52f506fb14502d28c8727174
Kirill Dlussky <kirill@dlussky.ru>
2017-10-03 16:20:06 +0300

    new versions

--- install_albert.sh
+++ install_albert.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 #installing albert from git
-ALBERT_VERSION="v0.8.10"
+ALBERT_VERSION="v0.12.0"
 sudo apt-get install -y g++ cmake qtbase5-dev libqt5x11extras5-dev libqt5svg5-dev libmuparser-dev
 git clone https://github.com/ManuelSchneid3r/albert.git /tmp/buildalbert
 cd /tmp/buildalbert
--- install_awesome.sh
+++ install_awesome.sh
@@ -2,9 +2,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+
+#https://packages.ubuntu.com/zesty/amd64/libxcb-xrm0/download
+#https://packages.ubuntu.com/zesty/amd64/libxcb-xrm-dev/download
 #installing awesome from git
-AWESOME_VERSION="v3.5.9"
-sudo apt-get install cmake lua5.2 glib-2.0 libcairo2-dev libx11-dev libxcb-* libgdk-pixbuf2.0-dev libx11-xcb-dev libxkbcommon-dev libxkbcommon-x11-dev libstartup-notification0-dev libxdg-basedir-dev liblua* libdbus-1-dev lua-lgi-dev 
+AWESOME_VERSION="v4.2"
+sudo apt-get install cmake lua5.2 glib-2.0 libcairo2-dev libx11-dev libxcb-* libgdk-pixbuf2.0-dev libx11-xcb-dev libxkbcommon-dev libxkbcommon-x11-dev libstartup-notification0-dev libxdg-basedir-dev liblua* libdbus-1-dev lua-lgi-dev
 git clone https://github.com/awesomeWM/awesome.git
 cd awesome
 git checkout $AWESOME_VERSION
