7912da3076610c049faab575dd0ab7b9e741ccb1
Kirill Dlussky <kirill@dlussky.ru>
2018-01-22 22:30:20 +0300

    albert install script cleanup

--- install_albert.sh
+++ install_albert.sh
@@ -2,18 +2,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-#installing albert from git
 ALBERT_VERSION="v0.14.14"
 
-CURRENT_DIR=`pwd`
 SRC_DIR=/tmp/albert_src
 BUILD_DIR=/tmp/albert_build
 
 sudo apt-get install -y g++ cmake qtbase5-dev libqt5x11extras5-dev libqt5svg5-dev libmuparser-dev qtdeclarative5-dev python3-dev 
 
 git clone --recursive https://github.com/ManuelSchneid3r/albert.git $SRC_DIR
-cd $SRC_DIR
-git checkout $ALBERT_VERSION
+git -C $SRC_DIR checkout $ALBERT_VERSION
 
 mkdir $BUILD_DIR
 cd $BUILD_DIR
@@ -22,6 +19,6 @@ cmake $SRC_DIR -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_VI
 make
 sudo make install
 
-cd $CURRENT_DIR
+cd ..
 rm -rf $SRC_DIR
 rm -rf $BUILD_DIR
