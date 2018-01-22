b0e9faac6090e14b8dacf200edadc385a7c3259b
Kirill Dlussky <kirill@dlussky.ru>
2018-01-22 21:50:23 +0300

    more elegant solution for disabling vbox plugin)

--- install_albert.sh
+++ install_albert.sh
@@ -15,35 +15,10 @@ git clone --recursive https://github.com/ManuelSchneid3r/albert.git $SRC_DIR
 cd $SRC_DIR
 git checkout $ALBERT_VERSION
 
-#removing virtualbox plugin
-cd plugins
-rm -rf virtualbox
-
-patch -b <<EOF
-
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index ce5090d..ef4716b 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -96,11 +96,6 @@ if (BUILD_TERMINAL)
-     add_subdirectory(terminal)
- endif()
- 
--option(BUILD_VIRTUALBOX "Build the extension" ON)
--if (BUILD_VIRTUALBOX)
--    add_subdirectory(virtualbox)
--endif()
--
- option(BUILD_WEBSEARCH "Build the extension" ON)
- if (BUILD_WEBSEARCH)
-     add_subdirectory(websearch)
-
-EOF
-
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
-cmake $SRC_DIR -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release
+cmake $SRC_DIR -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_VIRTUALBOX=Off
 make
 sudo make install
 
