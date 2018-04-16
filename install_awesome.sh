e7575250c60a7d3a09c78217118ab189bd946f02
Kirill Dlussky <kirill@dlussky.ru>
2018-04-16 16:20:02 +0300

    awesome install fix

--- install_awesome.sh
+++ install_awesome.sh
@@ -7,7 +7,7 @@ IFS=$'\n\t'
 #https://packages.ubuntu.com/zesty/amd64/libxcb-xrm-dev/download
 #installing awesome from git
 AWESOME_VERSION="v4.2"
-sudo apt-get install cmake lua5.2 glib-2.0 libcairo2-dev libx11-dev libxcb-* libgdk-pixbuf2.0-dev libx11-xcb-dev libxkbcommon-dev libxkbcommon-x11-dev libstartup-notification0-dev libxdg-basedir-dev liblua* libdbus-1-dev lua-lgi-dev
+sudo apt-get install cmake lua5.2 glib-2.0 libcairo2-dev libx11-dev libxcb-* libgdk-pixbuf2.0-dev libx11-xcb-dev libxkbcommon-dev libxkbcommon-x11-dev libstartup-notification0-dev libxdg-basedir-dev liblua5.2* libdbus-1-dev lua-lgi-dev
 git clone https://github.com/awesomeWM/awesome.git
 cd awesome
 git checkout $AWESOME_VERSION
