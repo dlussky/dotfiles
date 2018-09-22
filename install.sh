0676447d9a1311a19840d18016d5a9778558ceec
Kirill Dlussky <kirill@dlussky.ru>
2018-09-23 02:01:34 +0300

    removed obsolete scripts, updated install scripts

--- install.sh
+++ install.sh
@@ -2,27 +2,25 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-
-
-
-
 sudo add-apt-repository -y ppa:numix/ppa
-sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
-sudo add-apt-repository -y ppa:alexx2000/doublecmd
-sudo add-apt-repository -y ppa:flexiondotorg/albert 
-
+sudo add-apt-repository -y ppa:webupd8team/terminix
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -yV install chromium-browser redshift-gtk qbittorrent skype keepass2 virtualbox autossh compton mc guake albert php php-cli php-xml doublecmd-gtk sublime-text filezilla cabextract moc zsh tmux htop build-essential checkinstall openssh-server expect screenfetch numix-icon-theme
+sudo apt-get -yV install 													 \
+	adb albert autossh build-essential checkinstall chromium-browser compton \
+	doublecmd-gtk fastboot ffmpeg filezilla gparted htop iotop keepass2 	 \
+	mc moc numix-icon-theme openssh-server php-cli qbittorrent redshift-gtk  \
+	screenfetch sublime-text tilix tmux tree zsh awesome
 
-#freetype build-dep
-#sudo apt-get -y install fontconfig fontconfig-config libcairo-gobject2 libcairo-gobject2:i386 libcairo-script-interpreter2 libcairo2 libcairo2:i386 libcairo2-dev libfontconfig1 libfontconfig1:i386 libfontconfig1-dev libfreetype6:i386 libfreetype6 libfreetype6-dev
+./link_configs.sh
 
-#appies icons from freeflagicons.com
+./install_albert.sh
+./install_oh_my_zsh.sh
+./install_motd_rocket.sh
+./install_vmtouch.sh
+
+# creates additional symlink for icons
+ln -s ~/.local/share/icons/flags ~/.icons/flags
+# applies flag icons from freeflagicons.com
 dconf write /org/mate/desktop/peripherals/keyboard/indicator/show-flags "true"
-
-#infinality
-#sudo add-apt-repository -y ppa:rjvbertin/infinaltimate #1
-#sudo expect misc/fc-presets.expect #for rjvbertin
-#sudo add-apt-repository -y ppa:no1wantdthisname/ppa    #2
--- install_awesome.sh
+++ install_awesome.sh
@@ -2,16 +2,21 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-
-#https://packages.ubuntu.com/zesty/amd64/libxcb-xrm0/download
-#https://packages.ubuntu.com/zesty/amd64/libxcb-xrm-dev/download
 #installing awesome from git
 AWESOME_VERSION="v4.2"
+
+BUILD_DIR=/tmp/awesome
+
 sudo apt-get install cmake lua5.2 glib-2.0 libcairo2-dev libx11-dev libxcb-* libgdk-pixbuf2.0-dev libx11-xcb-dev libxkbcommon-dev libxkbcommon-x11-dev libstartup-notification0-dev libxdg-basedir-dev liblua5.2* libdbus-1-dev lua-lgi-dev imagemagick
-git clone https://github.com/awesomeWM/awesome.git
-cd awesome
+
+git clone https://github.com/awesomeWM/awesome.git $BUILD_DIR
+cd $BUILD_DIR
 git checkout $AWESOME_VERSION
+
 make
-sudo make install
+make package
+
+sudo dpkg -i build/awesome-*-Linux.deb
+
 cd ..
-rm -rf awesome
+rm -rf $BUILD_DIR
