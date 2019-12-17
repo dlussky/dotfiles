476b2a2d17700171d4b59ab9204e482fe40f7dfe
Kirill Dlussky <kirill@dlussky.ru>
2019-12-17 13:29:02 +0300

    Cpu governor configuration, sysctl tuning

--- install.sh
+++ install.sh
@@ -20,3 +20,13 @@ xargs -a misc/packages.list sudo apt install -yV
 ln -s ~/.local/share/icons/flags ~/.icons/flags
 # applies flag icons from freeflagicons.com
 dconf write /org/mate/desktop/peripherals/keyboard/indicator/show-flags "true"
+# applies performance cpu governer
+echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils
+sudo systemctl disable ondemand
+
+# bbr congestion control
+printf "net.core.default_qdisc=fq \nnet.ipv4.tcp_congestion_control=bbr" | sudo tee /etc/sysctl.d/42-congestion.conf
+# inotify watch limit
+echo 'fs.inotify.max_user_watches=524288' | sudo tee /etc/sysctl.d/42-inotify-watch-limit.conf
+# applies sysctl config
+sudo sysctl -p
