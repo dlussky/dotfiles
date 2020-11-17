f3ef892a07d728e453422fddcee9546f8105cfb6
Kirill Dlussky <kirill@dlussky.ru>
2020-11-17 21:32:08 +0300

    function for line deletion blame and alias for batch image optimization added

--- conf/.config/zsh/aliases.zsh
+++ conf/.config/zsh/aliases.zsh
@@ -8,3 +8,11 @@ alias tree="tree -vaC --dirsfirst"
 function ttd() {
   date -d @$1 +'%d.%m.%Y %H:%M:%S'
 }
+
+alias optijpg='find . -name "*.jpg" | xargs mogrify -auto-orient -resize 50%'
+
+function blamedelete() {
+  for commit in $(git log --pretty='%H'); do
+    git diff -U0 --ignore-space-change "$commit^" "$commit" | grep '^-.*$0' > /dev/null && echo "$commit"
+  done
+}
